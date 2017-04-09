a8de911302e1aef61ba849339e64affaa62677ef
Girish Pillai <gpillai@microsoft.com>
2017-04-09 17:26:21 -0500

    xx

---
+++ AzureStack/AzureStackPS/AzureStackPS/Add-VM-Images.ps1
@@ -0,0 +1,49 @@
+#
+# Add_VM_Images.ps1  
+# #region GitHub Utilities and templates
+invoke-webrequest https://github.com/Azure/AzureStack-Tools/archive/master.zip -OutFile "$env:TEMP\master.zip"
+expand-archive "$env:TEMP\master.zip" -DestinationPath C:\ -Force
+Remove-Item "$env:TEMP\master.zip"
+
+invoke-webrequest "https://github.com/Azure/AzureStack-QuickStart-Templates/archive/master.zip" -OutFile "$env:TEMP\master.zip"
+expand-archive "$env:TEMP\master.zip" -DestinationPath C:\ -Force
+Remove-Item "$env:TEMP\master.zip"
+#endregion
+
+#region Import Modules
+
+Import-Module C:\AzureStack-Tools-master\Connect\AzureStack.Connect.psm1
+Import-Module C:\AzureStack-Tools-master\ComputeAdmin\AzureStack.ComputeAdmin.psm1
+get-module azurerm* -listavailable
+get-module azurestack -listavailable 
+install-module azurestack -requiredversion 1.2.9
+
+invoke-webrequest "https://github.com/Azure/AzureStack-Tools/archive/master.zip" -OutFile "$env:TEMP\master.zip"
+expand-archive "$env:TEMP\master.zip" -DestinationPath C:\ -Force
+Remove-Item "$env:TEMP\master.zip"
+
+cd C:\AzureStack-Tools-master
+
+Login-AzureRmAccount
+invoke-webrequest https://partner-images.canonical.com/azure/azure_stack/ubuntu-14.04-LTS-microsoft_azure_stack-20170225-10.vhd.zip -OutFile "C:\Temp\Ubuntu.zip"
+cd C:\Temp
+expand-archive ubuntu.zip -DestinationPath . -Force
+Import-Module C:\AzureStack-Tools-master\ComputeAdmin\AzureStack.ComputeAdmin.psm1
+# Store the AAD service administrator account credentials in a variable 
+$UserName='sidlabadmin@clouddemoorg.onmicrosoft.com'
+$Password='$1D@zure$tackLab!'|ConvertTo-SecureString -Force -AsPlainText
+$MASCredential=New-Object PSCredential($UserName,$Password)
+$AadTenant = Get-AADTenantGUID -AADTenantName "clouddemoorg.onmicrosoft.com"
+
+## 2. Register an AzureRM environment that targets your Azure Stack instance
+# Use this command to access the administrative portal - BROKEN in TP3 Refresh
+ Add-AzureStackAzureRmEnvironment -ArmEndpoint "https://adminportal.local.azurestack.external" -Name "AzureStackAdmin" 
+
+ # Use this command to access the user portal
+ Add-AzureStackAzureRmEnvironment -Name "AzureStackUser" -ArmEndpoint "https://portal.local.azurestack.external"
+#Ubuntu - does not work
+#Add-VMImage -publisher "Canonical" -offer "UbuntuServer" -sku "14.04.3-LTS" -version "1.0.0" -osType Linux -osDiskLocal 'C:\Temp\trusty-server-cloudimg-amd64-disk1.vhd' -tenantID "$AADTenantDomain" -AzureStackCredentials $MASCredential -ArmEndpoint $AdminArmEndpoint
+Add-VMImage -publisher "Canonical" -offer "UbuntuServer" -sku "14.04.3-LTS" -version "1.0.0" -osType Linux -osDiskLocal 'C:\Temp\trusty-server-cloudimg-amd64-disk1.vhd' -tenantID "clouddemoorg.onmicrosoft.com" -AzureStackCredentials $MASCredential -ArmEndpoint "https://adminportal.local.azurestack.external"
+Add-VMImage -publisher "Canonical" -offer "UbuntuServer" -sku "14.04.3-LTS" -version "1.0.0" -osType Linux -osDiskLocal 'C:\Temp\trusty-server-cloudimg-amd64-disk1.vhd' -tenantID "clouddemoorg.onmicrosoft.com" -AzureStackCredentials $MASCredential -EnvironmentName "AzureStackAdmin"
+# Windows 2016
+New-Server2016VMImage -ISOPath $ISOPath -TenantId $AADTenantDomain -AzureStackCredentials $MASCredential -EnvironmentName 'AzureStackEnv' -IncludeLatestCU -Version Both -Net35
