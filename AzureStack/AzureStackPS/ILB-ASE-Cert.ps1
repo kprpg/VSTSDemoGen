8cdcffb6f9e9643bf9a7c11ce4b6e120355c2e39
Girish Pillai <gpillai@microsoft.com>
2017-08-27 17:13:20 -0500

    ss

---
+++ AzureStack/AzureStackPS/ILB-ASE-Cert.ps1
@@ -0,0 +1,7 @@
+﻿$certificate = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname "\*.myFabriKam.com","\*.scm.myFabriKam.com"
+
+$certThumbprint = "cert:\localMachine\my\" +$certificate.Thumbprint
+$password = ConvertTo-SecureString -String "P2ssw0rd" -Force -AsPlainText
+
+$fileName = "exportedcert.pfx" 
+Export-PfxCertificate -cert $certThumbprint -FilePath $fileName -Password $password
